aeef2f82987172e41aa3b83452f83543279c2038
CVSI <contact@CVSI.nc>
2026-06-05 18:13:51 +1100

    feature : ajout de PATCH

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -16,9 +16,9 @@ productRouter.get('/', (req, res) => {
 });
 
 productRouter.get('/:id', (req, res) => {
-    const pId = parseInt(req.params.id)
+    const pId = parseInt(req.params.id, 10)
     const product = products.find(p => p.id === pId)
-    return product ? res.json(product) : (res.status(404).json({ erreur: "Produit non trouvé" }));
+    return product ? res.json(product) : (res.status(404).json({ error: "Produit non trouvé" }));
 });
 
 productRouter.post("/", (req, res) => {
@@ -27,7 +27,7 @@ productRouter.post("/", (req, res) => {
         || product.name.trim() === ''
         || product.price <= 0
     )
-        return res.status(400).json({ erreur: "Données invalides" });
+        return res.status(400).json({ error: "Données invalides" });
 
     //recherche le nouvel id avant ajout.
     const newId = Math.max(...products.map(p => p.id), 0) + 1;
@@ -47,10 +47,37 @@ productRouter.post("/", (req, res) => {
     res.status(201).json(newProduct);
 });
 
+productRouter.patch('/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const productIndex = products.findIndex(p => p.id === id);
+    const updatedProduct: Partial<Product> = req.body;
+
+    //verifier que le produit existe
+    if (productIndex === -1) {
+        return res.status(404).json({ error: "Produit non trouvé" });
+    }
+
+    // Valider seulement les champs envoyés
+    if (updatedProduct.name !== undefined && updatedProduct.name.trim() === '') {
+        return res.status(400).json({ error: 'Nom invalide' });
+    }
+    if (updatedProduct.price !== undefined && updatedProduct.price <= 0) {
+        return res.status(400).json({ error: 'Prix invalide' });
+    }
+    if (updatedProduct.stock !== undefined && updatedProduct.stock < 0) {
+        return res.status(400).json({ error: 'Stock invalide' });
+    }
+
+    products[productIndex] = { ...products[productIndex], ...updatedProduct };
+
+    res.json(products[productIndex]);
+
+});
+
 productRouter.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id, 10);
     if (!products.find((p => p.id === id))) {
-        return res.status(404).json({ erreur: "Produit non trouvé" });
+        return res.status(404).json({ error: "Produit non trouvé" });
     }
     products = products.filter((p => p.id !== id));
     res.json({ message: `Produit ${id} supprimé` });
